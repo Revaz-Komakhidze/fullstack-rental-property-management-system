4f35fbc51652b9d106c72748823b03e128e59e15
Revaz Komakhidze <rezokomakhidze1@gmail.com>
2024-01-17 11:10:00 +0400

    added react icons library

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
-      <div className="flex items-center w-full md:2/3"></div>
+      <div className="flex items-center w-full md:2/3">
+        <Link href="/" className="font-black text-[#F27405]">
+          MyHome
+        </Link>
+      </div>
       <ul></ul>
     </header>
   );
